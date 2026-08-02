95aa71407c5f793be614fddb68f9ac3022c1b9d9
Luís Melo <luisevieiramelo@gmail.com>
2026-08-02 14:25:44 -0300

    Login de perfil com duas contas, dados separados por usuario e gravacao imediata ao sair

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'agenda-v1';
+const CACHE = 'agenda-v2';
 const ASSETS = ['./', './index.html', './manifest.webmanifest',
   './icons/icon-192.png', './icons/icon-512.png', './icons/icon-maskable-512.png'];
 
